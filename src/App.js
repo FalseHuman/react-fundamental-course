59c5233951a5f69a8b889413843e5bf9a4c7dd2a
FalseHuman <emilkhazioff@yandex.com>
2023-07-16 18:20:18 +0500

    base React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+
+
+function App() {
+  const [value, setValue] = useState('Text');
+
+  return (
+    <div className="App">
+      <h1>{value}</h1>
+      <input type='text' value={value} onChange={event => setValue(event.target.value)}/>
+      <Counter />
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
